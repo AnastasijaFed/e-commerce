51e1dd231f18d2f7a20b28f4dc6fbba64cb8e9ec
AnastasijaFed <nastefedorenko@gmail.com>
2025-10-16 18:30:17 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    colors: {
+      darkOlive: "#636B2F",
+      brightText: "#f5fdec",
+      darkText: "#636B2F",
+      componentBg: "#3D4127"
+    }
+  }
+}
